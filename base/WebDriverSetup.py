39c8c28e62cc41fa6c9103950cbe8573fd7b58d6
Ofir Carmel <ofir121995@gmail.com>
2022-03-01 10:56:58 +0200

    add docker capabilities

--- base/WebDriverSetup.py
+++ base/WebDriverSetup.py
@@ -10,27 +10,43 @@ import os
 cwd = os.getcwd()
 
 class WebDriverSetup:
-    @pytest.fixture()
-    def read_test_conf_data(self):
-        self.data = read_data("./base/test_conf.json")
-        self.website_url, self.browser = self.data[0]
+    @pytest.fixture(params=read_data("./base/test_conf.json"))
+    def read_test_conf_data(self, request):
+        self.website_url, self.browser, self.remote = request.param
 
     @pytest.fixture(autouse=True)
     def set_up(self, read_test_conf_data):
-        if self.browser.casefold() == "Chrome".casefold():
-            self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        elif self.browser.casefold() == "Firefox".casefold():
-            self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        elif self.browser.casefold() == "Internet Explorer".casefold():
-            self.driver = webdriver.Ie(executable_path=IEDriverManager().install())
-        elif self.browser.casefold() == "Opera".casefold():
-            self.driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+        if self.remote:
+            if self.browser.casefold() == "Chrome".casefold():
+                dc = webdriver.DesiredCapabilities.CHROME
+            elif self.browser.casefold() == "Firefox".casefold():
+                dc = webdriver.DesiredCapabilities.FIREFOX
+            elif self.browser.casefold() == "Internet Explorer".casefold():
+                dc = webdriver.DesiredCapabilities.INTERNETEXPLORER
+            elif self.browser.casefold() == "Opera".casefold():
+                dc = webdriver.DesiredCapabilities.OPERA
+            else:
+                raise ValueError("Invalid browser value")
+            self.driver = webdriver.Remote(
+                desired_capabilities=dc,
+                command_executor="http://localhost:4444/wd/hub",
+            )
         else:
-            raise ValueError("Invalid browser value")
+            if self.browser.casefold() == "Chrome".casefold():
+                self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+            elif self.browser.casefold() == "Firefox".casefold():
+                self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+            elif self.browser.casefold() == "Internet Explorer".casefold():
+                self.driver = webdriver.Ie(executable_path=IEDriverManager().install())
+            elif self.browser.casefold() == "Opera".casefold():
+                self.driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+            else:
+                raise ValueError("Invalid browser value")
 
         self.driver.implicitly_wait(10)
         self.driver.get(self.website_url)
-        self.driver.maximize_window()
+        if not self.remote:
+            self.driver.maximize_window()
         yield self.driver
         if self.driver is not None:
             self.driver.close()
--- pages/CartPage.py
+++ pages/CartPage.py
@@ -1,4 +1,3 @@
-
 from locators.locators import Locators
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
